94f8e8a14234a730e305e2d6bd6c8692a3bf1cf6
Sofi <20230106@ricaldone.edu.sv>
2024-09-27 11:09:48 -0600

    Inicio

---
+++ Pancito/src/main/java/com/mycompany/pancito/Pancito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pancito;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Pancito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
